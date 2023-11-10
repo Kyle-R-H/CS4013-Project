88bad1eec267c5f66f077c4774ef38872ebd10a9
KyleH <russelhellstrom@gmail.com>
2023-11-10 22:11:45 +0000

    added lab stuff to start terminal input

---
+++ StudentRecordsMenu.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StudentRecordsMenu {
+    private Scanner input;
+    
+    /**
+     * Runs System
+     */
+    public StudentRecordsMenu(){
+        input = new Scanner(System.in);
+    }
+    public void run(){
+        boolean running = true;
+
+        while (running) {
+            //do stuff in terminal if we doin terminal and not GUI
+        }
+    }
+}
