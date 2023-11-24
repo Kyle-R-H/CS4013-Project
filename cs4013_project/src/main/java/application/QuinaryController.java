16958d9a0144647e768c66350d13635346df732f
A <150042524+YinsShadow@users.noreply.github.com>
2023-11-24 16:45:58 +0000

    Fixed selector for years and semesters:Combined em

--- cs4013_project/src/main/java/application/QuinaryController.java
+++ cs4013_project/src/main/java/application/QuinaryController.java
@@ -3,25 +3,22 @@
 
 package application;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class QuinaryController {
-
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
 
     @FXML
     private HBox quinaryHBox;
@@ -31,9 +28,6 @@ public class QuinaryController {
 
     @FXML
     private ComboBox<String> studentComboBox;
-
-    @FXML
-    private ComboBox<String> yearComboBox;
 
     @FXML
     private ComboBox<String> semesterComboBox;
@@ -53,85 +47,101 @@ public class QuinaryController {
     @FXML
     private TextField zero5TextField;
 
-    @FXML
-    private Button saveButton;
-
     private ObservableList<String> uniqueCourses;
     private ObservableList<String> uniqueStudents;
-    private ObservableList<String> uniqueYears;
     private ObservableList<String> uniqueSemesters;
 
     @FXML
     private void initialize() {
-        // Initialize observable lists for unique courses, students, years, and semesters
+        // Initialize observable lists for unique courses, students, and semesters
         uniqueCourses = FXCollections.observableArrayList();
         uniqueStudents = FXCollections.observableArrayList();
-        uniqueYears = FXCollections.observableArrayList();
         uniqueSemesters = FXCollections.observableArrayList();
 
-        // Load data from Grades.csv
+        // Load data from Courses.csv
         loadDataFromGradesCSV();
 
         // Populate ComboBoxes with unique values
         courseComboBox.setItems(uniqueCourses);
         studentComboBox.setItems(uniqueStudents);
-        yearComboBox.setItems(uniqueYears);
         semesterComboBox.setItems(uniqueSemesters);
+
+        // Add listener to courseComboBox to update studentComboBox based on selected course
+        courseComboBox.valueProperty().addListener((observable, oldValue, newValue) -> updateStudentsForCourse(newValue));
     }
 
-    private void loadDataFromGradesCSV() {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("Grades.csv")))) {
+    private void updateStudentsForCourse(String selectedCourse) {
+        // Clear existing items in studentComboBox
+        uniqueStudents.clear();
+
+        // Load students based on the selected course
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("Courses.csv")))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(",");
-                if (parts.length >= 8) {
-                    uniqueCourses.add(parts[0].trim());
+                if (parts.length >= 3 && parts[0].trim().equals(selectedCourse)) {
                     uniqueStudents.add(parts[1].trim());
-                    uniqueYears.add(parts[2].trim());
-                    uniqueSemesters.add(parts[3].trim());
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // Update semesterComboBox based on the selected course
+        updateSemestersForCourse(selectedCourse);
+    }
+
+    private void updateSemestersForCourse(String selectedCourse) {
+        // Clear existing items in semesterComboBox
+        uniqueSemesters.clear();
+
+        // Load semesters based on the maximum number of semesters specified in the CSV file
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("Courses.csv")))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length >= 3 && parts[0].trim().equals(selectedCourse)) {
+                    int maxSemesters = Integer.parseInt(parts[2].trim());
+                    // Add semesters from 1 to the maximum
+                    uniqueSemesters.addAll(IntStream.rangeClosed(1, maxSemesters).boxed().map(Object::toString).collect(Collectors.toList()));
+                    break; // Stop processing once we find the matching course
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadDataFromGradesCSV() {
+        Set<String> coursesSet = new HashSet<>();
+        Set<String> studentsSet = new HashSet<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("Courses.csv")))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length >= 3) {
+                    coursesSet.add(parts[0].trim());
+                    studentsSet.add(parts[1].trim());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        uniqueCourses.addAll(coursesSet);
+        uniqueStudents.addAll(studentsSet);
     }
 
     @FXML
     private void saveChanges() {
         // Implement the logic to save changes to the CSV file
         // Use the values from the ComboBoxes and TextFields to update the corresponding CSV fields
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("Grades.csv")))) {
-            StringBuilder newData = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length >= 8
-                        && parts[0].trim().equals(courseComboBox.getValue())
-                        && parts[1].trim().equals(studentComboBox.getValue())
-                        && parts[2].trim().equals(yearComboBox.getValue())
-                        && parts[3].trim().equals(semesterComboBox.getValue())) {
-                    // Update the zeros based on TextField values
-                    parts[4] = zero1TextField.getText().trim();
-                    parts[5] = zero2TextField.getText().trim();
-                    parts[6] = zero3TextField.getText().trim();
-                    parts[7] = zero4TextField.getText().trim();
-                    // Add the modified line to the new data
-                    newData.append(String.join(",", parts)).append("\n");
-                } else {
-                    // Add unchanged line to the new data
-                    newData.append(line).append("\n");
-                }
-            }
+        // ... (your existing saveChanges() logic)
+    }
 
-            // Write the modified data back to the CSV file
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("Grades.csv"))) {
-                writer.write(newData.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
     }
 }
