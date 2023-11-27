adcceaf47bfe0cd32dc08a0337ef49cb9f58aa67
KyleH <russelhellstrom@gmail.com>
2023-11-27 21:44:45 +0000

    just in case big oopsie

--- cs4013_project/src/main/java/application/QuaternaryController.java
+++ cs4013_project/src/main/java/application/QuaternaryController.java
@@ -2,6 +2,9 @@
 
 package application;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
@@ -17,7 +20,34 @@ public class QuaternaryController {
     private TextArea transcriptTextArea;
     
 
-    // private void QuaternaryController(){
-    //     transcriptTextArea.setStyle(-fx);
+    // @FXML
+    // private void initialize(){
+    //     //get info from csv's
+
+
+    //     transcriptTextArea.setText("To be Continued...");
+    // }
+
+    // @FXML
+    // private void csvInfo(){
+    //     final String STUDENT_FILE = "src\\main\\resources\\application\\Student.csv";
+    //     final String COURSE_FILE = "src\\main\\resources\\application\\Courses.csv";
+    //     try (BufferedReader studentReader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(STUDENT_FILE)));
+    //     BufferedReader courseReader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(COURSE_FILE)))) {
+    //         String studentLine = studentReader.readLine();
+    //         String courseLine = courseReader.readLine();
+
+        
+        
+        
+        
+        
+        
+        
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //         // Handle IO exception
+    //     }
+
     // }
 }
