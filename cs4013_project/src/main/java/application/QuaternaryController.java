456ae2d8afc334da081d5a585a79c0eedda2448a
KyleH <russelhellstrom@gmail.com>
2023-11-27 23:38:27 +0000

    why wont setText() work :,(

--- cs4013_project/src/main/java/application/QuaternaryController.java
+++ cs4013_project/src/main/java/application/QuaternaryController.java
@@ -4,7 +4,7 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.Arrays;
 
 import java.io.IOException;
@@ -24,33 +24,49 @@ public class QuaternaryController {
 
     @FXML
     private void initialize(){
+        transcriptTextArea.setStyle(
+                "-fx-control-inner-background: transparent;" +
+                "-fx-font-weight: normal;" +
+                "-fx-text-fill: black; " +
+                "-fx-background-color: transparent; " +
+                "-fx-focus-color: transparent; " +
+                "-fx-faint-focus-color: transparent; " +
+                "-fx-border-color: transparent;"
+        );
+        transcriptTextArea.getParent().requestFocus(); // Set focus to another node to apply the styles immediately
         //get info from csv's
 
-        transcriptTextArea.setText("WHY");
+        transcriptTextArea.setText(csvInfo());
     }
 
     @FXML
     private String csvInfo(){
-        try (BufferedReader studentReader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("src\\main\\resources\\application\\Student.csv")));
-        BufferedReader courseReader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("src\\main\\resources\\application\\Courses.csv")))) {
+        final String STUDENT_FILE = "\\application\\Student.csv";
+        final String COURSE_FILE = "\\application\\Courses.csv";
+
+        try(BufferedReader studentReader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(STUDENT_FILE)));
+            BufferedReader  courseReader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(COURSE_FILE)))) {
+
             String studentLine = studentReader.readLine();
             String courseLine = courseReader.readLine();
-            while (studentLine != null && courseLine != null) {
-                String[] studentParts = studentLine.split(",");
+
+            while (studentLine != null && courseLine != null) { //while both files arent empty
+                String[] studentParts = studentLine.split(","); 
                 String[] courseParts = courseLine.split(",");
-                if (studentParts.length == 2 && studentParts[0].equals(SecondaryController.studentNumber) && studentParts[0].equals(courseParts[1])) {
+                if (studentParts[0].equals(SecondaryController.studentNumber) && studentParts[0].equals(courseParts[1]) && SecondaryController.studentNumber != null) {
                     String theString = "Student Parts: " + Arrays.toString(studentParts) + "\n" +
                     "Course Parts: " + Arrays.toString(courseParts);
                     return theString;
-                    //!insert the whole transcript into this one line
+                    //!insert the whole transcript info into this one line
+                } else{
+                    return "Something not equalling in CSV or courseNumber is NULL";
                 }
             }
-
-        
+            return "while loop looped";
         } catch (IOException e) {
             e.printStackTrace();
             // Handle IO exception
+            return "Caught something while fishing";
         }
-        return "Big oopsie";
     }
 }
